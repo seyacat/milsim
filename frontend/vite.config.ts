5f111943324a06848019aeb0ee716e016de6eae1
Seyacat <seyacat@gmail.com>
2025-10-19 13:00:42 -0500

    Remove legacy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../src/public2',
+    outDir: '../src/public',
     emptyOutDir: true
   },
   server: {
