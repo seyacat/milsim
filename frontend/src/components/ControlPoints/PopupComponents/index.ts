f1d8568de0affbb75df5dd13e3fbd346ec804811
Seyacat <seyacat@gmail.com>
2025-10-26 11:49:10 -0500

    frontend react removed

--- frontend/src/components/ControlPoints/PopupComponents/index.ts
+++
@@ -1,6 +0,0 @@
-export { OwnerPopup } from './OwnerPopup';
-export { PlayerPopup } from './PlayerPopup';
-export { PositionChallenge } from './PositionChallenge';
-export { CodeChallenge } from './CodeChallenge';
-export { BombChallenge } from './BombChallenge';
-export { PopupRenderer, createPopupContent } from './PopupRenderer';
